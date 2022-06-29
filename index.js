f06df2dbdc0000b4b413ae00f85cb6b68eba15a0
imran-hossain <csebubt35@gmail.com>
2022-06-29 11:36:25 +0600

    get6 api create

--- index.js
+++ index.js
@@ -13,7 +13,28 @@ require('dotenv').config()
 const port = process.env.PORT || 5000
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3s80f.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+      await client.connect();
+      const productCollection = client.db("mango_jelly").collection('allPhone');
+     
+   
+       
+ 
+      app.get('/products', async (req, res) => {
+        const result = await productCollection.find().toArray()
+        res.send(result)
+  
+      })
+       
+    } finally {
+  
+    }
+  }
+  run().catch(console.dir);
+  
 app.get('/', (req, res) => {
     res.send('Welcome to mango jelly ')
   })
